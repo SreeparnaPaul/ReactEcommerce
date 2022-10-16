8525f2b023dc7a99cef7a331c50f8ce2f6a49942
SreeparnaPaul <sreeparnapaul21@gmail.com>
2022-10-16 11:44:29 +0530

    changes in navbar,app.js,index.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,38 @@
 import React from 'react'
 import {FaSignInAlt,FaShoppingCart,FaUserPlus} from 'react-icons/fa'
+import {NavLink} from "react-router-dom";
+
 function Navbar() {
   return (
     <div>
     <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm">
     <div className="container">
-      <a className="navbar-brand fw-bold fs-4" href="#">SHOPPER'S Collection</a>
+      <NavLink className="navbar-brand fw-bold fs-4" to="/">SHOPPER'S Collection</NavLink>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
           </li>
           <li className="nav-item">
-          <a className="nav-link" href="#">Products</a>
+          <NavLink className="nav-link" to="/products">Products</NavLink>
         </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
+            <NavLink className="nav-link" to="/about">About</NavLink>
           </li>
           <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
         </li>
         </ul>
         <div className='buttons'>
-      <a href='' className='btn btn-outline-dark'>
-     <FaSignInAlt className='me-1'/>Login</a>
-     <a href='' className='btn btn-outline-dark ms-2'>
-     <FaUserPlus className='me-1 '/>Register</a>
-     <a href='' className='btn btn-outline-dark ms-2'>
-     <FaShoppingCart className='me-1'/>Cart(0)</a>
+      <NavLink to='/login' className='btn btn-outline-dark'>
+     <FaSignInAlt className='me-1'/>Login</NavLink>
+     <NavLink to='/register' className='btn btn-outline-dark ms-2'>
+     <FaUserPlus className='me-1 '/>Register</NavLink>
+     <NavLink to='/cart' className='btn btn-outline-dark ms-2'>
+     <FaShoppingCart className='me-1'/>Cart(0)</NavLink>
         </div>
       </div>
     </div>
